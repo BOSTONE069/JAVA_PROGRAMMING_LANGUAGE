7d944a8ea522ec0ce57fb0d8742ea5dcddbae281
BOSTONE069 <bostoneochieng69@gmail.com>
2023-08-31 16:16:30 +0300

    Handling exceptions

---
+++ ExceptionLearning.java
@@ -0,0 +1,14 @@
+public class ExceptionLearning {
+
+    public static void main(String[] args) {
+        try {
+            // Block pf code to try
+            int a = 11;
+            int b = 0;
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            // Block of code to nhandle error code
+            System.out.println("The number cannot divided by zero.");
+        }
+    }
+}
