657edfff34191338096badc5909d933f4ae82b51
BOSTONE069 <bostoneochieng69@gmail.com>
2023-08-31 11:09:31 +0300

    Exception Handling: try and catch

---
+++ Index.java
@@ -0,0 +1,12 @@
+public class Index {
+    public static void main(String[] args){
+        try {
+            int [] myNumbers = {1,2,3,4,5,6,7,8};
+            System.out.println(myNumbers[8]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong in numbers array;" + e);
+        }
+        System.out.println("That was easy!");
+
+    }
+}
