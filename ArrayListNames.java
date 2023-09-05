ddf9008f33ffa7392841584cb5e237b3acca9fd8
BOSTONE069 <bostoneochieng69@gmail.com>
2023-09-05 10:19:49 +0300

    Data structure Array List Names

---
+++ ArrayListNames.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListNames {
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+
+        names.add("David");
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Samir");
+        names.add("David");
+
+        int lastDavid = names.lastIndexOf("David");
+
+        System.out.println("Last David at " + lastDavid);
+    }
+}
