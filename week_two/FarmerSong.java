e685f042922dbb2923b4f45a8b8d871670199dfb
BOSTONE069 <bostoneochieng69@gmail.com>
2023-12-18 16:27:28 +0300

    Writing methoda in Java

---
+++ week_two/FarmerSong.java
@@ -0,0 +1,17 @@
+package week_two;
+
+public class FarmerSong {
+	
+	public static void refrain() {
+		System.out.println("The farmer in the dell");
+	}
+
+	public static void main(String[] args) {
+		refrain();
+		refrain();
+		System.out.println("Heigh ho the derry-o");
+		refrain();
+
+	}
+
+}
