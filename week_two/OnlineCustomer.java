5fff94c57fd710007db39080c3a55bed33ef4bd1
BOSTONE069 <bostoneochieng69@gmail.com>
2023-12-20 17:04:54 -0800

    Using the super keyword ny arguments and by the contructor

---
+++ week_two/OnlineCustomer.java
@@ -0,0 +1,15 @@
+package week_two;
+
+public class OnlineCustomer extends Customer {
+    private String email;
+
+    public OnlineCustomer(String n, String a, String e) {
+        super(n, a);
+        email = e;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nEmail: " + email;
+    }
+}
