773468859b95a2124bc7b69b57b4215bff5d9e5b
BOSTONE069 <bostoneochieng69@gmail.com>
2023-12-26 15:32:43 -0800

    Modified the variable name for the list

--- week_three/QueueExample.java
+++ week_three/QueueExample.java
@@ -6,26 +6,26 @@ import java.util.Queue;
 public class QueueExample {
 
 	public static void main(String[] args) {
-		Queue<String> languages = new LinkedList<>();
+		Queue<String> namesForTheQueue = new LinkedList<>();
 
 	    // add elements
-	    languages.add("Python");
-	    languages.add("Java");
-	    languages.add("C");
-	    System.out.println("LinkedList: " + languages);
+		namesForTheQueue.add("Python");
+		namesForTheQueue.add("Java");
+		namesForTheQueue.add("C");
+	    System.out.println("LinkedList: " + namesForTheQueue);
 
 	    // access the first element
-	    String str1 = languages.peek();
+	    String str1 = namesForTheQueue.peek();
 	    System.out.println("Accessed Element: " + str1);
 
 	    // access and remove the first element
-	    String str2 = languages.poll();
+	    String str2 = namesForTheQueue.poll();
 	    System.out.println("Removed Element: " + str2);
-	    System.out.println("LinkedList after poll(): " + languages);
+	    System.out.println("LinkedList after poll(): " + namesForTheQueue);
 
 	    // add element at the end
-	    languages.offer("Swift");
-	    System.out.println("LinkedList after offer(): " + languages);
+	    namesForTheQueue.offer("Swift");
+	    System.out.println("LinkedList after offer(): " + namesForTheQueue);
 	  }
 
 }
