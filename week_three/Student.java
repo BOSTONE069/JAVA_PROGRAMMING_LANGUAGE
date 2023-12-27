0d18f65e632090ae7097cb0215bf967d1c84cad8
BOSTONE069 <bostoneochieng69@gmail.com>
2023-12-26 16:43:40 -0800

    Added comments in the code

--- week_three/Student.java
+++ week_three/Student.java
@@ -2,16 +2,19 @@ package week_three;
 
 public class Student {
 	
-	private String name;
-	private String email;
-	private int id;
-	
-	public Student(String initName, String initEmail, int initId){
+	/**
+ * This is a Java program that defines a Student class with private variables for name, email, and id.
+ * It includes a constructor to initialize the variables and a toString() method to return a string representation of the Student object.
+ */
+private String name;
+private String email;
+private int id;
 
-		this.name = initName;
-        this.email = initEmail;
-        this.id = initId;
-    }
+public Student(String initName, String initEmail, int initId){
+    this.name = initName;
+    this.email = initEmail;
+    this.id = initId;
+}
 
     public String toString(){
         return "Name: " + name + " Email: " + email + " ID: " + id;
--- week_three/StudentList.java
+++ week_three/StudentList.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 
 public class StudentList {
 
-    public static void main(String[] args){
-        ArrayList<Student> roster = new ArrayList<Student>();
-        roster.add(new Student("William", "willian@gmail.com", 20));
-        roster.add(new Student("John", "john@gmail.com", 21));
-        roster.add(new Student("Velma", "joy@gmail.com", 22));
-        roster.add(new Student("Joy", "joy@gmail.com", 23));
+    /**
+ * This code demonstrates the usage of an ArrayList to store a list of Student objects.
+ * It creates a roster of students, prints the total number of students, and then
+ * prints each student's details. Finally, it prints the entire roster.
+ */
 
-        System.out.println("The roster has " + roster.size() + " students.");
-
-        for (Student student : roster) {
-            System.out.println(student);
-        }
-
-        System.out.println(roster);
-
+public static void main(String[] args){
+    ArrayList<Student> roster = new ArrayList<Student>();
+    roster.add(new Student("William", "willian@gmail.com", 20));
+    roster.add(new Student("John", "john@gmail.com", 21));
+    roster.add(new Student("Velma", "joy@gmail.com", 22));
+    roster.add(new Student("Joy", "joy@gmail.com", 23));
+    System.out.println("The roster has " + roster.size() + " students.");
+    for (Student student : roster) {
+        System.out.println(student);
     }
+    System.out.println(roster);
+}
 }
