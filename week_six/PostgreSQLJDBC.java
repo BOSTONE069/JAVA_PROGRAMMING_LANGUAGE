6695c1f8d06f0375f12051be890e68f6d6d8cfae
BOSTONE069 <bostoneochieng69@gmail.com>
2024-02-06 09:34:37 +0300

    This is way of connecting the database for Postgreql

---
+++ week_six/PostgreSQLJDBC.java
@@ -0,0 +1,22 @@
+package week_six;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgreSQLJDBC {
+
+	public static void main(String[] args) {
+		Connection c = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/feedApp", "postgres", "pg123");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName()+": "+e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database Successfully");
+
+	}
+
+}
